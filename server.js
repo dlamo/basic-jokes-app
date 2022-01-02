2d3bc5f101ac2ac73e97f8c062fde718580f3931
David de Lamo <d.lamo.valverde@gmail.com>
2022-01-02 18:09:54 +0100

    eod

--- server.js
+++ server.js
@@ -1,39 +1,33 @@
-// const express = require("express")
-// const fetch = require('node-fetch')
-// const path = require("path")
-// const ejs = require('ejs')
 import express from "express"
 import dotenv from 'dotenv'
 import fetch from "node-fetch"
-import path from 'path'
-import ejs from 'ejs'
 
-// const jokes = [
-//   {
-//     "id": 1,
-//     "type": "general",
-//     "setup": "What did the fish say when it hit the wall?",
-//     "punchline": "Dam."
-//   },
-//   {
-//     "id": 2,
-//     "type": "general",
-//     "setup": "How do you make a tissue dance?",
-//     "punchline": "You put a little boogie on it."
-//   },
-//   {
-//     "id": 3,
-//     "type": "general",
-//     "setup": "What's Forrest Gump's password?",
-//     "punchline": "1Forrest1"
-//   },
-//   {
-//     "id": 4,
-//     "type": "general",
-//     "setup": "What do you call a belt made out of watches?",
-//     "punchline": "A waist of time."
-//   }
-// ]
+const jokes = [
+  {
+    "id": 1,
+    "type": "general",
+    "setup": "What did the fish say when it hit the wall?",
+    "punchline": "Dam."
+  },
+  {
+    "id": 2,
+    "type": "general",
+    "setup": "How do you make a tissue dance?",
+    "punchline": "You put a little boogie on it."
+  },
+  {
+    "id": 3,
+    "type": "general",
+    "setup": "What's Forrest Gump's password?",
+    "punchline": "1Forrest1"
+  },
+  {
+    "id": 4,
+    "type": "general",
+    "setup": "What do you call a belt made out of watches?",
+    "punchline": "A waist of time."
+  }
+]
 
 dotenv.config()
 const app = express()
@@ -43,12 +37,14 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 app.get("/*", (req, res) => {
-  let jokes = []
-  fetch(process.env.API_URL)
-    .then(response => response.json())
-    .then(data => console.log(data))
+  // let jokes = []
+  // fetch(process.env.API_URL)
+  //   .then(response => response.text())
+  //   .then(data => console.log(data))
+
   const randomIndex = Math.floor(Math.random() * jokes.length)
   const joke = jokes[randomIndex]
+  
   res.render('pages/index', { joke })
 })
 
